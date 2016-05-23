398247ecdb580f1fdd105e0586951c0d664756fb
CedricBongaerts <cedric.bongaerts@hotmail.com>
2016-05-23 18:04:54 +0000

    HTML5Mode

--- server.js
+++ server.js
@@ -1,16 +1,10 @@
-// Birdspotter
-// Author: Cedric Bongaerts
-// NodeJS Server
-
 // Init Express Web Framework
 var express = require('express');
 var app = express();
 var path = require('path');
 
-
 // Set view engine to EJS & set views directory
-app.engine('html', require('ejs').renderFile);
-app.set('view engine', 'html');
+app.set('view engine', 'ejs');
 app.set('views', path.resolve(__dirname, 'client', 'views'));
 
 app.use(express.static(path.resolve(__dirname, 'client')));
@@ -18,7 +12,6 @@ app.use(express.static(path.resolve(__dirname, 'client')));
 // Database Connection
 var mongoose = require('mongoose');
 var configDB = require('./server/config/database.js');
-var captures = require('./server/routes/capture')
 mongoose.connect(configDB.url);
 
 var bodyParser = require('body-parser');
@@ -29,17 +22,25 @@ app.use(bodyParser.json({ type: 'application/json'}));
 
 // Main route
 app.get('/', function(req, res){
-    res.render('index.html');
+    res.render('index.ejs');
 });
+
+// // catch 404 and forwarding to error handler
+// app.use(function(req, res, next) {
+//     var err = new Error('Not Found');
+//     err.status = 404;
+//     next(err);
+// });
+
 // API 
 var api = express.Router();
 require('./server/routes/capture')(api);
 app.use('/api', api);
 
-// // Set routes to other pages
-// app.get('/*', function(req, res){
-//     res.render('index.ejs');
-// });
+// Set routes to other pages
+app.get('/*', function(req, res){
+    app.use(express.static(path.join(__dirname, 'client')));
+});
 
 // Port Settings
 app.listen(process.env.PORT || 3000, process.env.IP);
